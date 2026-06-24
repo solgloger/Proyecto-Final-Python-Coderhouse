dcc89b32500191b0dc5430758bd1e1113eccd2e9
Sol Gloger <solgloger@gmail.com>
2026-06-24 20:37:04 -0300

    Definición de modelos Author, Tag, Post

--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.exceptions import ValidationError 
 
 # Create your models here.
+
+class Author (models.Model):
+    name = models.CharField (max_length=50)
+    email = models.EmailField (unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag (models.Model):
+    name = models.CharField (max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post (models.Model):
+    title = models.CharField (max_length=150)
+    content = models.TextField()
+    published_date = models.DateTimeField (auto_now_add=True)
+    
+    author = models.ForeignKey(
+        Author,
+        on_delete= models.CASCADE
+    )
+
+    tags = models.ManyToManyField(
+        Tag
+    )
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-published_date']
+
+    def clean(self):
+        if len(self.title) < 5:
+            raise ValidationError(
+                 {'Error: El título debe tener más de cinco caracteres'}
+        )
+
