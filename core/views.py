7e53e57819da645a6b09c793336ffe5e2b4fb181
Sol Gloger <solgloger@gmail.com>
2026-07-01 09:38:37 -0300

    Vistas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
-
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+class PostListView (ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return (
+            Post.objects
+            .filter(
+                published_date__isnull=False
+            )
+            .order_by("-published_date")
+        )
+
+class PostDetailView (DetailView):
+    model = Post 
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView (CreateView):
+    model = Post 
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    success_url = reverse_lazy(
+        "post_list"
+    )
+
+class PostUpdateView (UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    success_url = reverse_lazy(
+        "post_list"
+    )
+
+class PostDeleteView (LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy(
+        "post_list"
+    )
