d9c0e7a270c27d7a0f9d58a34673008518e179c8
Sol Gloger <solgloger@gmail.com>
2026-07-02 16:35:01 -0300

    Home

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", HomeView.as_view(), name="home"),
     path('post/', include("core.urls"))
 ]
--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import Author, Post, Tag
 # Register your models here.
 
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 1
+
 @admin.register (Author)
 class AuthorAdmin (admin.ModelAdmin):
     list_display = ('name', 'email')
     search_fields = ('name',)
+    inlines = [PostInline]
 
 @admin.register (Tag)
 class TagAdmin (admin.ModelAdmin):
@@ -17,5 +22,6 @@ class PostAdmin (admin.ModelAdmin):
     list_display = ('title', 'author', 'published_date')
     search_fields = ('title', 'author__name', 'tags__name')
     list_filter = ('published_date', 'author')
+    filter_horizontal = ("tags",)
 
 
